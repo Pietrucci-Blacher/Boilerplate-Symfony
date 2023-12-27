ff0c8596f40bfc28c960427a88c7968f0047c071
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-12-27 11:58:52 +0100

    Remove pentatrion old packages and update composer.lock

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
 
+/* if you're using React */
+// import react from '@vitejs/plugin-react';
+
 export default defineConfig({
     plugins: [
+        /* react(), // if you're using React */
         symfonyPlugin(),
     ],
-    server: {
-        watch: {
-            usePolling: true,
-        }
-    },
     build: {
         rollupOptions: {
             input: {
